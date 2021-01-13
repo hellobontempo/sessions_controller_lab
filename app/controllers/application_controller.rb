acb5c1e6a797c84af3d366dd860aefa8cfbba2e6
claire <clairebontempo@gmail.com>
2021-01-12 16:27:34 -0800

    commit tree 7ad1e3add568084dabf30cb6348dd63c5585111b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
  
   def hello
-    if !session[:name]
-      redirect_to '/login'
-    end
+    redirect_to '/login' unless session[:name]
+      #redirect_to '/login'
+      #redirect_to controller: 'sessions', action: 'new' unless session[:name]
+    #end
   end
+
 end
